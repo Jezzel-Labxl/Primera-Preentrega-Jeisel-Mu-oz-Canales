86d88508d1b238d1f04a0a4c7d6e1c822e2ed90c
Jezzel <JezzelxM@gmail.com>
2023-01-31 23:59:29 -0500

    Eliminando comentarios innecesarios

--- ciclo/javascript/miarchivo.js
+++ ciclo/javascript/miarchivo.js
@@ -1,5 +1,3 @@
-Programa que me calcula el total a pagar de productos seleccionados
-
 let producto = prompt("Ingrese el  Producto a Comprar");
 while (producto != "Salir") {
   let precio = prompt("Ingrese el precio unitario del Producto: " + producto);
